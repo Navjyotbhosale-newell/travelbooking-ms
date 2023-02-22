017f504d44c69111083dcb38e34b7c92f1fe3e18
NavjyotGit <navjyotbhosale@gmail.com>
2023-02-22 23:21:40 +0530

    initialcommit

---
+++ src/main/java/com/example/travelbookingms/flight.java
@@ -0,0 +1,12 @@
+package com.example.travelbookingms;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+@RestController
+public class flight {
+    @GetMapping("/myflight")
+    public String getData() {
+        return "Please book your flight tickets";
+    }
+}
